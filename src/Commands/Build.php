8839c34d47f7f4c3dcc0702077d3e4331406c62d
Ezinwa Okpoechi <brainmaestro@outlook.com>
2017-07-01 09:31:41 +0100

    Add decryption during env building

--- src/Commands/Build.php
+++ src/Commands/Build.php
@@ -3,6 +3,9 @@
 namespace BrainMaestro\Envman\Commands;
 
 use BrainMaestro\Envman\Parser;
+use Defuse\Crypto\Crypto;
+use Defuse\Crypto\Key;
+use Defuse\Crypto\Exception\WrongKeyOrModifiedCiphertextException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
@@ -23,23 +26,60 @@ class Build extends Command
                 'Directories to search for .env.* files',
                 ['.']
             )
+            ->addOption(
+                'key',
+                'k',
+                InputOption::VALUE_REQUIRED,
+                'Key file for encryption'
+            )
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $directories = $input->getArgument('directories');
+        $keyFile = $input->getOption('key');
+
         $env = Parser::parse($directories);
         $file = fopen('.env', 'w+');
+        if ($keyFile && is_file($keyFile)) {
+            $key = Key::loadFromAsciiSafeString(file_get_contents($keyFile));
+        }
 
-        foreach ($env->all() as $key => $entry) {
-            if ($env->isComment($key)) {
+        $count = 0;
+        foreach ($env->all() as $envKey => $entry) {
+            if ($env->isComment($envKey)) {
+                $output->writeln(
+                    "<fg=white>Skipped {$envKey} ({$entry['file']}) - Commented out</>",
+                    OutputInterface::VERBOSITY_VERBOSE
+                );
                 continue;
             }
 
-            fwrite($file, "{$key}={$entry['value']}\n");
+            if ($env->isEncrypted($envKey)) {
+                if (!isset($key)) {
+                    $output->writeln(
+                        "<fg=red>Skipped {$envKey} ({$entry['file']}) - No key to decrypt</>",
+                        OutputInterface::VERBOSITY_VERBOSE
+                    );
+                    continue;
+                }
+
+                try {
+                    $entry['value'] = Crypto::decrypt($entry['value'], $key);
+                } catch (WrongKeyOrModifiedCiphertextException $e) {
+                    $output->writeln('<error>The key you provided is incorrect</error>');
+                    return 1;
+                }
+
+                $envKey = str_replace('$', '', $envKey);
+            }
+
+            $count++;
+            fwrite($file, "{$envKey}={$entry['value']}\n");
         }
 
         fclose($file);
+        $output->writeln("Built <info>{$count}</info> environment variable(s)");
     }
 }
--- tests/Commands/BuildTest.php
+++ tests/Commands/BuildTest.php
@@ -3,6 +3,8 @@
 namespace BrainMaestro\Envman\Tests\Commands;
 
 use BrainMaestro\Envman\Commands\Build;
+use BrainMaestro\Envman\Commands\Encrypt;
+use BrainMaestro\Envman\Commands\GenerateKey;
 use BrainMaestro\Envman\Tests\TestUtil;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -16,6 +18,11 @@ class BuildTest extends TestCase
     public function setUp()
     {
         $this->commandTester = new CommandTester(new Build);
+    }
+
+    public function tearDown()
+    {
+        unlink('.env');
     }
 
     /**
@@ -34,7 +41,6 @@ class BuildTest extends TestCase
         );
 
         $this->delete('.', 'app', 'auth');
-        unlink('.env');
     }
 
     /**
@@ -55,7 +61,6 @@ class BuildTest extends TestCase
 
         $this->delete('.', 'auth');
         $this->delete('staging', 'app');
-        unlink('.env');
     }
 
     /**
@@ -73,6 +78,51 @@ class BuildTest extends TestCase
         );
 
         $this->delete('.', 'auth');
-        unlink('.env');
+    }
+
+    /**
+     * @test
+     */
+    public function it_decrypts_and_builds_encrypted_values_if_a_key_is_provided()
+    {
+        file_put_contents('.env.app', "APP_NAME=env-test-app");
+        (new CommandTester(new GenerateKey))->execute(['key-name' => 'env-test-key']);
+        (new CommandTester(new Encrypt))->execute(['--key' => 'env-test-key']);
+
+        $this->commandTester->execute(['--key' => 'env-test-key']);
+
+        $this->assertContains('Built 1 environment variable(s)', $this->commandTester->getDisplay());
+
+        $this->delete('.', 'app');
+        unlink('env-test-key');
+    }
+
+    /**
+     * @test
+     */
+    public function it_ignores_encrypted_values_if_no_key_is_provided()
+    {
+        file_put_contents('.env.app', "\$APP_NAME=env-test-app");
+
+        $this->commandTester->execute([]);
+
+        $this->assertContains('Built 0 environment variable(s)', $this->commandTester->getDisplay());
+
+        $this->delete('.', 'app');
+    }
+
+    /**
+     * @test
+     */
+    public function it_fails_if_an_incorrect_key_is_provided()
+    {
+        file_put_contents('.env.app', "\$APP_NAME=env-test-app");
+        (new CommandTester(new GenerateKey))->execute(['key-name' => 'env-test-key']);
+        $this->commandTester->execute(['--key' => 'env-test-key']);
+
+        $this->assertContains('The key you provided is incorrect', $this->commandTester->getDisplay());
+
+        $this->delete('.', 'app');
+        unlink('env-test-key');
     }
 }
